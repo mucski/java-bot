e5c1d4ef1845e0a1ea5aac571b74da03f718a394
mucski <music166@gmail.com>
2021-03-13 21:27:11 +0100

    Update help.js

--- commands/utilities/help.js
+++ commands/utilities/help.js
@@ -1,8 +1,3 @@
-	name: 'help',
-	description: 'List all of my commands or info about a specific command.',
-	aliases: ['commands'],
-	usage: '[command name]',
-	cooldown: 5,
 	execute(message, args) {
 		const data = [];
 		const { commands } = message.client;
