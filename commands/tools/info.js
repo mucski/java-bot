5bb61a41c37cf087b302526151f325973e6cd6e4
mucski <music166@gmail.com>
2021-03-13 22:04:45 +0100

    Create info.js

---
+++ commands/tools/info.js
@@ -0,0 +1,19 @@
+module.exports = {
+	name: 'info',
+	description: 'Information about the bot, and its owner.',
+	execute(message) {
+        const exampleEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Some title')
+        .setURL('https://discord.js.org/')
+        .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+        .setDescription(`${message.client.user.name}\nVersion:${version}`)
+        .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+        .addField('Inline field title', 'Some value here', true)
+        .setImage('https://i.imgur.com/wSTFkRM.png')
+        .setTimestamp()
+        .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+    
+    channel.send(exampleEmbed);
+	},
+};
