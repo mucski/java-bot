2baadcea51b7ac8ea8bee54de224ef903a446acc
mucski <music166@gmail.com>
2021-03-15 13:55:40 +0100

    test

--- commands/tools/activity.js
+++ commands/tools/activity.js
@@ -5,7 +5,7 @@ module.exports = {
     permission: 'OWNER',
 	execute(message, args) {
 	    if (!args[0]) {
-	        message.client.setActivity(args)
+	        message.client.user.setActivity(args)
 	        message.reply(`I have set my activity to ${args}`)
 	    } else {
 	        message.reply(`could not set my activity to nothing.`)
