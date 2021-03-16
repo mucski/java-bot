138f7e603e1ad611f88d4debe5de4c8206cc73d3
mucski <music166@gmail.com>
2021-03-16 15:42:55 +0100

    test

--- commands/utilities/avatar.js
+++ commands/utilities/avatar.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'avatar',
     description: 'Shows a users avatar or profile picture.',
     aliases: ['pfp'],
+    usage: '<nickname>',
     execute(message, args) {
         if (args[0]) {
             const tagged = message.mentions.users.first();
