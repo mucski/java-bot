732b23fcc3a7c96a3e9d23b0c16e34d74ff77d66
mucski <music166@gmail.com>
2021-03-16 09:16:31 +0100

    added avatar

---
+++ commands/utilities/avatar.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'avatar',
+    description: 'Shows a users avatar or profile picture.',
+    aliases: ['pfp'],
+    execute(message, args) {
+        if (args[0]) {
+            const tagged = message.mentions.users.first();
+            message.channel.send(`${tagged.user.displayAvatarURL({format: 'png', dynamic: true})}`);
+        } else {
+            message.channel.send(`${message.author.displayAvatarURL({format: 'png', dynamic: true})}`);
+        }
+    },
+};
